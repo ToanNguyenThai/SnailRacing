d81b34aab1a97c6bd5edb225d388b98ada7d5a49
ToanNguyenThai <19522366@gm.uit.edu.vn>
2022-02-07 11:07:31 +0700

    deploy

---
+++ pages/component/header/nav/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function Navbar() {
+    return (
+
+        <div className='header container'>
+            <img src='/img/header/SnailLogo1.png'></img>
+            <nav>
+                <img src='/img/header/navBG.png'></img>
+            </nav>
+        </div>
+
+
+
+    );
+}
+
+export default Navbar;
---
+++ pages/component/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './header'
+import section from './section'
+import footer from './footer'
+function Layout() {
+    return (
+
+        <Header></Header>
+
+
+    );
+}
+
+export default Layout;
