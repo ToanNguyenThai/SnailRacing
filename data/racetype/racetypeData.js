ba513572972511a7ed5181d7e9152e967c56d177
DT0998 <duongkytrieuvn@gmail.com>
2022-02-07 16:56:19 +0700

    update

---
+++ data/racetype/racetypeData.js
@@ -0,0 +1,6 @@
+export const racetypeData = [
+    {
+        title:'CHALLENGE RACE',
+        content:['Spend SNT to buy ticket to join this race','Winnner will receive an amount of SNT','The rest will receive 1 Medal as participation gift']
+    }
+]
